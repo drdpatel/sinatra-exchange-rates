f5275e5e6c60c47c7564dd562ffaa30b9a67279a
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-27 19:54:23 +0000

    currencies

--- app.rb
+++ app.rb
@@ -6,9 +6,10 @@ get("/") do
   
   @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=6069b11e44c9f01bfbd63f1f29366f15")
 
-  @string_response = @raw_resopones.to_s
+  @string_response = @raw_response.to_s
 
   @parsed_response = JSON.parse(@string_response)
-  
+
+  @currencies = @parsed_response.fetch("currencies")
   erb(:homepage)
 end

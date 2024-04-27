b12c286647ada07b9bb93d6ab2326525a82838fa
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-27 19:47:15 +0000

    parse

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ require "http"
 
 get("/") do
   
-  HTTP.get("https://api.exchangerate.host/list?access_key=6069b11e44c9f01bfbd63f1f29366f15")
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=6069b11e44c9f01bfbd63f1f29366f15")
+
+  @string_response = @raw_resopones.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+  
   erb(:homepage)
 end

5d17a8486d11d2ffd0b3bd32ce355c3fa9e90592
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-30 17:04:49 +0000

    semi-final

--- app.rb
+++ app.rb
@@ -35,5 +35,15 @@ get("/:from_currency/:to_currency") do
   @from = params.fetch("from_currency")
   @to = params.fetch("to_currency")
 
+  @url = "https://api.exchangerate.host/convert?acces_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}&from=#{@from}&to=#{@to}&amount=1"
+
+  @raw_response = HTTP.get(@url)
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @amount = @parsed_response.fetch("result")
+  
   erb(:step_two)
 end
